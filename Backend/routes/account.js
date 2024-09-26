afd19c95e224953501e0afc26d7b6acbdbb5beb3
Anuj Dubey <anujd2408@gmail.com>
2024-09-26 20:05:42 +0530

    Added

--- Backend/routes/account.js
+++ Backend/routes/account.js
@@ -11,3 +11,35 @@ router.get("/balance", async(req,res)=>{
         balance: account.balance
     })
 });
+
+router.post("/transfer" , async(req,res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    const account = await Account.findOne({userID: req.userID}).session(session);
+
+    if(!account || account.balance < amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient balance"
+        });
+    }
+
+    const toAccount = await Account.findOne({ userID: to }).session(session);
+
+    if(!toAccount){
+        res.status(400).json({
+            message : "Invalid Account"
+        });
+    }
+
+    await Account.updateOne({ userID: req.userID }, { $inc :{ balance: -amount } }).session(session);
+    await Account.updateOne({ userID: to }, { $inc: { balance: amount} }).session(session);
+
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer Successful"
+    });
+}); 
