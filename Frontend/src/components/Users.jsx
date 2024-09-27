151d77e183a7170f6579f1841dbb216c2026062b
Anuj Dubey <anujd2408@gmail.com>
2024-09-27 12:03:01 +0530

    added

--- Frontend/src/components/Users.jsx
+++ Frontend/src/components/Users.jsx
@@ -5,20 +5,23 @@ import axios from "axios";
 export const Users = () => {
     // Replace with backend call
     const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
 
     useEffect(()=>{
-        axios.get("http://localhost:3000/api/v1/user/bulk")
+        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
         .then(response => {
             setUsers(response.data.user)
         })
-    },[])
+    },[filter])
 
     return <>
         <div className="font-bold mt-6 text-lg">
             Users
         </div>
         <div className="my-2">
-            <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
+            <input onChange={(e) =>{
+                setFilter(e.target.value)
+            }} type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
         </div>
         <div>
             {users.map(user => <User user={user} />)}
@@ -42,7 +45,9 @@ function User({user}) {
         </div>
 
         <div className="flex flex-col justify-center h-ful">
-            <Button label={"Send Money"} />
+            <Button onClick={(e) =>{
+                history.pushState(`/send?id=` + user._id + "&name=" + user.firstName);
+            }} label={"Send Money"} />
         </div>
     </div>
 }
