2ee055a720fb9d72259fbc9995d15fb59badc3af
Anuj Dubey <anujd2408@gmail.com>
2024-11-10 11:18:01 +0530

    Added

--- Frontend/config-overrides.js
+++ Frontend/config-overrides.js
@@ -1,22 +1,22 @@
-import {require} from "webpack";
+const webpack = require("webpack");
 
 module.exports = function override(config, env) {
-    //do stuff with the webpack config...
+    // Modify webpack config with fallbacks and plugins
     config.resolve.fallback = {
         ...config.resolve.fallback,
         stream: require.resolve("stream-browserify"),
         buffer: require.resolve("buffer"),
-    }
-    config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"]
+    };
+    
+    config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"];
+    
     config.plugins = [
         ...config.plugins,
         new webpack.ProvidePlugin({
             process: "process/browser",
             Buffer: ["buffer", "Buffer"],
         }),
-    ]
-    // console.log(config.resolve)
-    // console.log(config.plugins)
+    ];
 
-    return config
-}
+    return config;
+};
