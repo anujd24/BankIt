4a88bd788db9fcd42ce5a7f6fb73abf6ea20ef77
Anuj Dubey <anujd2408@gmail.com>
2024-11-11 12:50:57 +0530

    Added

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,14 +1,13 @@
+import React from 'react';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client'; // Import from 'react-dom/client' for React 18
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from './App.jsx'; // Make sure App is correctly exported as a named or default export
+import { App } from './App.jsx';
 import './index.css';
 
-// Get the root element where your React app will be rendered
 const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-// Create a root and render the app
-const root = createRoot(rootElement); // Use createRoot for React 18+
 root.render(
   <StrictMode>
     <BrowserRouter>
