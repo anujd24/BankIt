29f8d8d8210f621f67357afaee87472edb48ff31
Anuj Dubey <anujd2408@gmail.com>
2024-11-11 12:31:56 +0530

    Added

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,27 +1,7 @@
-import { defineConfig } from 'vite';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeModulesPolyfillPlugin(),
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      // Optional: Alias any Node.js modules for browser compatibility
-      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
-    },
-  },
-  define: {
-    // Define global variables for Buffer
-    'global': 'globalThis',
-    'process.env': {},
-  }
-});
+  plugins: [react()],
+})
